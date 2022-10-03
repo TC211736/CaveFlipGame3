0d1db9624299599152ecbba0ad8c00e320c218f6
TC211736 <TC211736@reigate.ac.uk>
2022-10-03 12:48:29 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- core/src/com/mygdx/game/LoginScreen.java
+++ core/src/com/mygdx/game/LoginScreen.java
@@ -4,49 +4,22 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+
 
 
 public class LoginScreen extends ScreenAdapter {
     MyGdxGame game;
-    protected Stage stage;
+    Stage stage;
     SpriteBatch batch;
     Skin skin;
 
     public LoginScreen(MyGdxGame game) {
         this.game = game;
-
-        batch = new SpriteBatch();
-        //atlas = new TextureAtlas("uiskin.atlas");
-        skin = new Skin(Gdx.files.internal("uiskin.json"));
-        //skin.addRegions(atlas);
-
-        stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-
-        TextButton playButton = new TextButton("Play", skin);
-        playButton.setPosition(300, 300);
-        playButton.setSize(300, 60);
-
-        stage.addActor(playButton);
-
-        /*
-        camera = new OrthographicCamera();
-
-
-        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
-        camera.update();
-
-        */
 
     }
 
@@ -64,17 +37,28 @@ public class LoginScreen extends ScreenAdapter {
             }
         });
 
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        batch = new SpriteBatch();
+        skin = new Skin(Gdx.files.internal("flatEarthFiles/flat-earth-ui.json"));
+
+
+        TextButton playButton = new TextButton("Play", skin);
+        playButton.setPosition(300, 300);
+        playButton.setSize(300, 60);
+
+        stage.addActor(playButton);
+    }
+
+    @Override
+    public void render(float delta) {
+
         Gdx.gl.glClearColor(0, 0.25f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
-        /*mainframe = new Table();
-        mainframe.setFillParent(true);
-
-        mainframe.top();
-
-
-        TextButton optionsButton = new TextButton("Options", skin);
-        TextButton exitButton = new TextButton("Exit", skin); */
+        stage.act(delta);
+        stage.draw();
     }
 }
